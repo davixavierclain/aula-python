14cae06baaa7218fe05074fd12462c2f7787b682
Davixavier <dx.abreu@gmail.com>
2021-09-19 19:28:30 -0300

    aula07

---
+++ Aula07/atividade012.py
@@ -0,0 +1,3 @@
+v = float(input('Digite algum valor para algum produto: R$'))
+d = v - (v * 5 / 100)
+print ('O produto com 5% de desconto vale {}'.format(d))
---
+++ Aula07/atividade013.py
@@ -0,0 +1,4 @@
+s = float(input('Você recebeu um novo empregado, de um salário para ele: R$'))
+print ('Ele trabalhou bastante, então você dará 15% a mais esse mês')
+v = s + (s * 15 / 100)
+print ('Com o acrecimo de 15% ele irá receber R${}'.format(v))
