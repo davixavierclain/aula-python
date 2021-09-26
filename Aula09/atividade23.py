71f2cb4a62200d93f5db52f6c76af52a18edb582
Davixavier <dx.abreu@gmail.com>
2021-09-26 12:00:41 -0300

    Aula09

---
+++ Aula09/atividade23.py
@@ -0,0 +1,10 @@
+nu = int (input('Informe um número: '))
+u = nu // 1 % 10
+d = nu // 10 % 10
+c = nu // 100 % 10
+m = nu // 1000 % 10
+print ('Analizando {}'.format(nu))
+print ('A unidade é {}'.format(u))
+print ('A dezena é {}'.format(d))
+print ('A centena é {}'.format(c))
+print ('O milhar é {}'.format (m))
---
+++ Aula09/atividade26.py
@@ -0,0 +1,4 @@
+f = str(input('Digite uma frase: ')).strip().upper()
+print('A letra "A" aparece {} vezes nessa frase'.format(f.count('A')))
+print('A mesma letra aparece pela primeira vez na posição {}'
+      ' e pela última na {}'.format(f.find('A')+1,f.rfind('A')+1))
